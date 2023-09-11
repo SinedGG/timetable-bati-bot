4868f1b754e070248c026ab0b117b40412bcbebb
SinedGG <denis2320032@gmail.com>
2023-09-11 18:11:29 +0300

    Update timetable-bati-bot.config.js

--- timetable-bati-bot.config.js
+++ timetable-bati-bot.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "timetable-bati-bot",
+      name: "bati-bot",
       cwd: "/home/ubuntu/pm/timetable-bati-bot",
       script: "index.js",
-      watch: true,
-      watch_delay: 5000,
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      ignore_watch: ["temp"],
 
       env: {
         TG_TOKEN: "",
